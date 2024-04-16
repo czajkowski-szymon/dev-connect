82aee0665b64ccfc2659d6acefb1334dabceaf83
Szymon Czajkowski <sczajkowski83@gmail.com>
2024-04-16 18:41:41 +0200

    flyway db migration added with sample users data

---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,72 @@
+create table if not exists public.user_
+(
+    id               serial
+        primary key,
+    email            varchar(255)
+        constraint user_email_unique
+            unique,
+    enabled          boolean not null,
+    first_name       varchar(255),
+    locked           boolean not null,
+    password         varchar(255),
+    profile_image_id varchar(255)
+        constraint profile_image_id_unique
+            unique,
+    role             varchar(255)
+        constraint user__role_check
+            check ((role)::text = ANY ((ARRAY ['USER'::character varying, 'ADMIN'::character varying])::text[]))
+);
+
+alter table public.user_
+    owner to root;
+
+create table if not exists public.project
+(
+    id                 serial
+        primary key,
+    description        varchar(255),
+    project_name       varchar(255),
+    project_manager_id integer
+        constraint fkpaaciy8iaxjvhhxtx5eksrn7f
+            references public.user_
+            on update cascade on delete cascade
+);
+
+alter table public.project
+    owner to root;
+
+create table if not exists public.task
+(
+    id         serial
+        primary key,
+    body       varchar(255),
+    deadline   date,
+    is_done    boolean not null,
+    project_id integer
+        constraint fkk8qrwowg31kx7hp93sru1pdqa
+            references public.project
+            on update cascade on delete cascade,
+    user_id    integer
+        constraint fk6uc2pbxr1jfrgqtxlefkcshil
+            references public.user_
+            on update cascade on delete set null
+);
+
+alter table public.task
+    owner to root;
+
+create table if not exists public.user_project
+(
+    user_id    integer not null
+        constraint fk9fphmq6fwsavmphyn3qgi2qol
+            references public.project
+            on update cascade on delete cascade,
+    project_id integer not null
+        constraint fk6ow7l7mg8e7gp1ss10xn5ij2o
+            references public.user_
+            on update cascade on delete cascade
+);
+
+alter table public.user_project
+    owner to root;
+
---
+++ src/main/resources/db/migration/V2__insert_sample_users.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.user_ (id, email, enabled, first_name, locked, password, profile_image_id, role)
+VALUES (1, 'szymon@mail.com', true, 'Szymon', false, '$2a$10$LZ77KKq3JhYQYDiK6L2ZQeVUhDXyGemd6zrrAndn1hHZLdZ1M2m/i', null, 'ADMIN');
+
+INSERT INTO public.user_ (id, email, enabled, first_name, locked, password, profile_image_id, role)
+VALUES (2, 'jakub@mail.com', true, 'Jakub', false, '$2a$10$zB62jI0jZO0UggyyGyi38.HsDo2n.9SPmLohgBQyAroMjZvKfg3x6', null, 'USER');
+
+INSERT INTO public.user_ (id, email, enabled, first_name, locked, password, profile_image_id, role)
+VALUES (3, 'maciej@mail.com', true, 'Maciej', false, '$2a$10$IU9WNgOYcmTTaCZEPqdFq.WtlkrWtndlzJype2azbA9CzCMNuipbu', null, 'USER');
+
+INSERT INTO public.user_ (id, email, enabled, first_name, locked, password, profile_image_id, role)
+VALUES (4, 'mateusz@mail.com', true, 'Mateusz', false, '$2a$10$WR2.qng9rEW8MUiAljPaMetDAytYphww3GOHZ0V5iAgqeAuhvEB.i', null, 'USER');
+
+INSERT INTO public.user_ (id, email, enabled, first_name, locked, password, profile_image_id, role)
+VALUES (5, 'kamil@mail.com', true, 'Kamil', false, '$2a$10$aBl7ipLlHFsGUnHlUMuoF.XVhUhLh9NNb3jhkloqnzfhVf5KRDEdS', null, 'USER');
